e77452ff58d6e5408fec07f7236bce17f92b6a60
Ankit kumar <82726735+ankit3710@users.noreply.github.com>
2021-06-19 19:57:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var input=document.getElementById("input");
+var pen=document.getElementById("pen");
+var ul = document.querySelector("ul");
+function createList(){
+    var li =document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    var dbtn = document.createElement("button");
+    dbtn.appendChild(document.createTextNode("X"));
+    li.appendChild(dbtn);
+    input.value="";
+    dbtn.onclick=function(){
+        li.classList.add("delete")
+    }
+    li.onclick=function(){
+        li.classList.toggle("done")
+    }
+    
+};
+pen.onclick=function(){
+    createList()
+};
+    
+
